253955abe45d7d41e497d5c382b10eba56a6aed7
Caleb Denio <cjdenio44@gmail.com>
2021-01-02 20:51:24 -0500

    :bug: maybe?

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 
 INSTALL_OS=$(uname)
 
-curl -o emojicode.tar.gz -L "https://github.com/emojicode/emojicode/releases/download/v$INPUT_VERSION/Emojicode-$INPUT_VERSION-$INSTALL_OS-x86_64.tar.gz"
+curl -o emojicode.tar.gz -L "https://github.com/emojicode/emojicode/releases/download/v$EMOJI_VERSION/Emojicode-$EMOJI_VERSION-$INSTALL_OS-x86_64.tar.gz"
 tar -xzf emojicode.tar.gz && rm emojicode.tar.gz
-cd Emojicode-$INPUT_VERSION-$INSTALL_OS-x86_64
+cd Emojicode-$EMOJI_VERSION-$INSTALL_OS-x86_64
 sudo mkdir -p /usr/local/include/emojicode && sudo mv include/* /usr/local/include/emojicode
 sudo mkdir -p /usr/local/EmojicodePackages && sudo mv packages/* /usr/local/EmojicodePackages
 sudo mv emojicodec /usr/local/bin/emojicodec
-cd .. && rm -r Emojicode-$INPUT_VERSION-$INSTALL_OS-x86_64
+cd .. && rm -r Emojicode-$EMOJI_VERSION-$INSTALL_OS-x86_64
