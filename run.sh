55f557bc5e3fa0b76b97de37e9e2a17fd9f22906
Caleb Denio <cjdenio44@gmail.com>
2021-01-02 20:30:35 -0500

    :bug: Fix a bunch of things

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ $OSTYPE == "linux_gnu" ]]
+then
+  INSTALL_OS = "Linux"
+elif [[ $OSTYPE == "darwin" ]]
+then
+  INSTALL_OS = "Darwin"
+else
+  echo "Invalid OS :("
+  return 1
+fi
+
+curl -o emojicode.tar.gz -L "https://github.com/emojicode/emojicode/releases/download/v$INPUT_VERSION/Emojicode-$INPUT_VERSION-$INSTALL_OS-x86_64.tar.gz"
+tar -xzf emojicode.tar.gz && rm emojicode.tar.gz
+cd Emojicode-$INPUT_VERSION-$INSTALL_OS-x86_64
+sudo mkdir -p /usr/local/include/emojicode && sudo mv include/* /usr/local/include/emojicode
+sudo mkdir -p /usr/local/EmojicodePackages && sudo mv packages/* /usr/local/EmojicodePackages
+sudo mv emojicodec /usr/local/bin/emojicodec
+cd .. && rm -r Emojicode-$INPUT_VERSION-$INSTALL_OS-x86_64
